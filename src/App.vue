e3acca9c5777af00af53ce37348304173b971a5a
codewithnatan <natangraboo@gmail.com>
2026-06-27 12:48:24 +0200

    feat: implement conditional rendering using v-if, v-else-if and v-else

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,31 @@ function decreaseCounter() {
    <p>Aktuelle Klicks {{ counter }}</p>
 
    <!-- Ein Event-Klick: Löst die Funktion aus -->
-    <button @click="increaseCounter">Klick mich!</button>
+    <button @click="increaseCounter">Plus (+)</button>
 
     <!-- Ein Event-Klick: Löst die Funktion aus -->
-     <button @click="decreaseCounter">Löschen</button>
+     <button @click="decreaseCounter">Minus (-)</button>
+
+     <!-- Hierr startet das WENN/DANN Prinzip -->
+
+     <div class="message-container">
+
+      <!-- Wenn der Zähler größer oder gleich 5 ist -->
+       <p v-if="counter >= 5" class="warning">
+        Achtung: Du hast 5 oder mehr Klicks erreicht!
+       </p>
+
+       <!-- Wenn der Zähler kleiner 0 ist -->
+        <p v-else-if="counter < 0" class="warning">
+          Du bist im minus Bereich!
+        </p>
+
+       <!-- Wenn die obere Bedingung NICHT zutrifft -->
+        <p v-else class="info">
+          Alles im grünen Bereich. Klicke weiter!
+        </p>
+
+     </div>
 
   </main>
 </template>
@@ -38,4 +59,7 @@ main { font-family: sans-serif; text-align: center; margin-top: 5%; }
 h1 { color: #42b883; }
 p { font-size: 1.5rem; }
 button { padding: 10px 20px; margin: 5px; font-size: 1rem; background-color: #42b883; color: white; border: none; border-radius: 5px; cursor: pointer; }
+.message-container {margin-top: 30px; height: 50px;}
+.warning {color: red; font-weight: bold;}
+.info {color: #666; font-style: italic;}
 </style>
